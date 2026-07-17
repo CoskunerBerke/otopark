04c51ee1ad713be45541cf4d53876dad53eb3a8f
CoskunerBerke <coskunerberke@gmail.com>
2026-07-17 10:04:53 +0300

    Updated contact page address and Google Maps location

--- src/config/business.ts
+++ src/config/business.ts
@@ -16,9 +16,9 @@ export const businessConfig = {
   city: "Ankara",
 
   googleMapsUrl:
-    "https://www.google.com/maps/search/?api=1&query=Prestij+Business+Center+Ostim+100.+Y%C4%B1l+Blv+No:55+Yenimahalle+Ankara",
+    "https://www.google.com/maps/place/Ostim+Prestij+%C4%B0%C5%9F+Merkezi/@39.9744439,32.7479707,17z/data=!3m1!4b1!4m6!3m5!1s0x14d34789df50e505:0xe21f57bc83f069a5!8m2!3d39.9744439!4d32.7505456!16s%2Fg%2F11b8df_z0z",
   googleMapsEmbedUrl:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3059.123456!2d32.757!3d39.978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMznCsDU4JzQwLjgiTiAzMsKwNDUnMjUuMiJF!5e0!3m2!1str!2str!4v1700000000000!5m2!1str!2str",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3058.423985108398!2d32.7479707!3d39.9744439!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34789df50e505%3A0xe21f57bc83f069a5!2sOstim%20Prestij%20%C4%B0%C5%9F%20Merkezi!5e0!3m2!1str!2str!4v1710000000000!5m2!1str!2str",
 
   parentCompany: {
     name: "TRİ Metal Yapı",
